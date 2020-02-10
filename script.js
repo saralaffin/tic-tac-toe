242e086a3d3265f0e5bbc729d94ed58fcf231b19
Sara Laffin <sarala@umich.edu>
2020-02-10 12:28:43 -0500

    hard code win if first row is selected and no other boxes

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let main = document.querySelector("main")
 let boxContainer = document.querySelector(".boxes")
 let playerHeading = document.querySelector(".player")
 function populateBoard(){
-    for (let i = 0; i < 9; i++) {
-        let box = document.createElement("div")
-        box.setAttribute("class","box")
-        box.addEventListener("click", colorChange)
-        boxContainer.appendChild(box)
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let box = document.createElement("div")
+            box.setAttribute("class","box")
+            box.setAttribute('data-row', i)
+            box.setAttribute('data-column', j)
+            box.addEventListener("click", colorChange)
+            boxContainer.appendChild(box)
+        }
     }
 }
 populateBoard()
@@ -18,22 +22,37 @@ reset.addEventListener("click", resetBoard)
 main.appendChild(reset)
 let resetButton = document.querySelector(".reset")
 
-let players = [{
-    color: "red"
-}, {
-    color: "blue"
-}]
-let currentPlayer = 0
-playerHeading.innerText = `It is ${players[currentPlayer].color}'s turn`
-function colorChange(eve) {
-    eve.target.style.backgroundColor = players[currentPlayer].color
-    eve.target.removeEventListener("click", colorChange)
-    if (currentPlayer == 0) {
-        currentPlayer = 1
-    } else {
-        currentPlayer = 0
+let players = {
+    red:{
+        color: "red",
+        board: [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ]
+    },
+    blue:{
+        color: "blue",
+        board: [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ]
     }
-    playerHeading.innerText = `It is ${players[currentPlayer].color}'s turn`
+}
+let currentPlayer = players.red
+playerHeading.innerText = `It is ${currentPlayer.color}'s turn`
+function colorChange(eve) {
+    eve.target.style.backgroundColor = currentPlayer.color
+    eve.target.removeEventListener("click", colorChange)
+    if (currentPlayer == players.red) {
+        currentPlayer = players.blue
+    } else {
+        currentPlayer = players.red
+    }
+    playerHeading.innerText = `It is ${currentPlayer.color}'s turn`
+    currentPlayer.board[parseInt(eve.target.dataset.row)][parseInt(eve.target.dataset.column)] = 1
+    checkWin(currentPlayer.board)
 }
 
 function resetBoard(eve) {
@@ -42,4 +61,16 @@ function resetBoard(eve) {
     boxContainer.setAttribute("class","boxes")
     populateBoard()
     main.insertBefore(boxContainer,resetButton)
+}
+
+let winComboFirstRow = [
+    [1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+function checkWin(comboArray) {
+    if (JSON.stringify(comboArray)==JSON.stringify(winComboFirstRow)) {
+        console.log("Player wins!")
+    }
 }
